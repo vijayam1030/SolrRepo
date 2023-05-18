6c83d64bbe164d3b5f3022ca03c84ff0c16df017
vijayam1030 <115277577+vijayam1030@users.noreply.github.com>
2023-05-18 14:52:39 -0400

    Update App.java

--- App.java
+++ App.java
@@ -28,3 +28,30 @@ public class MainClass {
         // Add your code here
     }
 }
+
+
+
+//// case
+public class MainClass {
+    public static void main(String[] args) {
+        // Check if there are any input arguments
+        if (args.length > 0) {
+            // Get the first input argument
+            String input = args[0];
+
+            // Trigger a method based on the input argument
+            switch (input) {
+                case "method1":
+                    method1();
+                    break;
+                case "method2":
+                    method2();
+                    break;
+                default:
+                    System.out.println("Invalid input argument");
+                    break;
+            }
+        } else {
+            System.out.println("No input arguments provided");
+        }
+    }
