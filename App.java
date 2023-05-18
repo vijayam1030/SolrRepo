aac10ab67e9df02deb7b901221d42179309964e9
vijayam1030 <115277577+vijayam1030@users.noreply.github.com>
2023-05-18 14:49:38 -0400

    Create App.java

---
+++ App.java
@@ -0,0 +1,30 @@
+public class MainClass {
+    public static void main(String[] args) {
+        // Check if there are any input arguments
+        if (args.length > 0) {
+            // Get the first input argument
+            String input = args[0];
+
+            // Trigger a method based on the input argument
+            if (input.equals("method1")) {
+                method1();
+            } else if (input.equals("method2")) {
+                method2();
+            } else {
+                System.out.println("Invalid input argument");
+            }
+        } else {
+            System.out.println("No input arguments provided");
+        }
+    }
+
+    public static void method1() {
+        System.out.println("Method 1 triggered");
+        // Add your code here
+    }
+
+    public static void method2() {
+        System.out.println("Method 2 triggered");
+        // Add your code here
+    }
+}
